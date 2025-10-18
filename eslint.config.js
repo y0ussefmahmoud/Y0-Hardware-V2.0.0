3011e948d0b6a66c15056461073f50181c860143
_y0ussef__ <youssef11mahmoud112002@gmail.com>
2025-10-18 06:02:03 +0300

    fix vite build config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,5 @@
 export default [
   {
-    ignores: ['dist', 'node_modules'],
-  },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'warn',
-      'react/prop-types': 'off',
-    },
-  },
+    ignores: ['dist', 'node_modules', '*.config.js'],
+  }
 ]
